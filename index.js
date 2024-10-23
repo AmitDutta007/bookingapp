3b97e8249bc4c7bdf3462c0e1219140409068803
amitdutta <duttaamit356@gmail.com>
2024-10-23 11:23:10 +0530

    connected to mongoDb

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log('connected in DB');
+        
+    } catch (error) {
+        throw error
+    }
+}
+
 
 app.listen(3000, () => {
+    connect()
     console.log("Connected to express !!!");
-
 })
 
 // app.get("/", () => {
